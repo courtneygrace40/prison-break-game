8e00fc57ee4e0dfbf8296381780c9b8507b099b8
ashakulp <ashakulp@localhost>
2026-04-02 20:35:19 -0400

    updated challengeRoom class

--- src/rooms/ChallengeRoom.java
+++ src/rooms/ChallengeRoom.java
@@ -1,5 +1,22 @@
 package rooms;
 
+
+import main.KeyHandler;
+import main.Modify_Frame;
+
 public class ChallengeRoom extends Room{
+	
+	public ChallengeRoom(Modify_Frame mf, KeyHandler kh, int x, int y, String filename) {
+		
+		this.setImage(filename);
+		this.playerx = x;
+		this.playery = y;
+		this.mf = mf;
+		this.kh = kh;
+		
+	}
+	
+	
+	
 	
 }
--- src/rooms/Room.java
+++ src/rooms/Room.java
@@ -1,5 +1,12 @@
 package rooms;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import main.KeyHandler;
 import main.Modify_Frame;
 
@@ -8,5 +15,33 @@ public class Room {
 	KeyHandler kh;
 	int playerx; //where the character spawns at first
 	int playery;
+	public BufferedImage rm;
+	
+	
+	public void setImage(String filename) {
+		try{
+			rm = ImageIO.read(getClass().getResourceAsStream(filename));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		// Source - https://stackoverflow.com/a/9417836
+		// Posted by Ocracoke, modified by community. See post 'Timeline' for change history
+		// Retrieved 2026-02-11, License - CC BY-SA 3.0
+			System.out.println(filename + " is being set up.");
+		    Image tmp = rm.getScaledInstance(mf.frameWidth, mf.frameHeight, Image.SCALE_SMOOTH);
+		    rm = new BufferedImage(mf.frameWidth, mf.frameHeight, BufferedImage.TYPE_INT_ARGB);  
+		    Graphics2D g2d = rm.createGraphics();
+		    g2d.drawImage(tmp, 0, 0, null);
+		    g2d.dispose();
+	}
+	
+	public void draw(Graphics2D g2) {
+		BufferedImage image = rm;
+		g2.drawImage(image, 0, 0, null);
+		
+	}
+	
+	
 	
 }
