4ea0daf348579e3b64ed3ce871c72e34499e0849
kawsarabdisalan <kawsarabdisalan@localhost>
2026-04-24 23:35:14 -0400

    Recommitting combat room

--- src/rooms/CombatRoom.java
+++ src/rooms/CombatRoom.java
@@ -3,13 +3,36 @@ package rooms;
 import main.KeyHandler;
 import main.Modify_Frame;
 
-public class CombatRoom extends Room{
+public class CombatRoom {
+	public int playerHealth;
+	public int guardHealth;
+	
+	public CombatRoom() {
+		playerHealth = 40;
+		guardHealth = 40;
 
-	private static final long serialVersionUID = 1L;
-
-	public CombatRoom(Modify_Frame mf, KeyHandler kh, String f, boolean lastFrame) {
-		super(mf, kh, f, lastFrame);
-		// TODO Auto-generated constructor stub
 	}
+	public void playerAttack() {
+		guardHealth -= 5;
+	}
+	
+	public void guardAttack() {
+		playerHealth -= 5;
+	}
+	
+	public boolean isOver() {
+		return playerHealth <= 0 || guardHealth <= 0;
+	}
+	
+	public boolean playerWon() {
+		return guardHealth <= 0;
+	}
+	
+	public boolean guardWon() {
+		return playerHealth <= 0;
+	}
+	
+
 
 }
+
